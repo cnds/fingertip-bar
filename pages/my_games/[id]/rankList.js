3af882e27445c4cd4198ba997f8cd8bee6f91c28
Lancer-xu <a995810193@gmail.com>
2022-06-25 15:40:12 +0800

    feat: :sparkles: 排行榜列表收起

--- pages/my_games/[id]/rankList.js
+++ pages/my_games/[id]/rankList.js
@@ -1,10 +1,27 @@
+import ArrowDown from "@/public/arrow_down.svg";
 import RankingsBg from "@/public/rankings_bg.svg";
 import RedPacketIcon from "@/public/red_packet_icon.svg";
 import TrophyGold from "@/public/trophy_gold.svg";
 import classNames from "classnames";
+import { useMemo, useState } from "react";
+import { Collapse } from "react-collapse";
 import styles from "./rankings.module.scss";
 
-const RankList = ({ rankListData }) => {
+const RankList = ({ rankListData, type }) => {
+  const [isOpened, setIsOpened] = useState(false);
+
+  const fixedRankList = useMemo(() => {
+    return rankListData.slice(0, 2);
+  }, [rankListData]);
+
+  const collapsibleRankList = useMemo(() => {
+    return rankListData.slice(2);
+  }, [rankListData]);
+
+  const handleToggleIsOpened = () => {
+    setIsOpened((isOpened) => !isOpened);
+  };
+
   return (
     <div className={styles.box}>
       <RankingsBg className={styles.rankingsBg} />
@@ -14,7 +31,13 @@ const RankList = ({ rankListData }) => {
           <span>奖励</span>
           <em>3000.0</em>
         </span>
-        <span className={styles.rankingName}>等级排行榜一</span>
+        <span className={styles.rankingName}>
+          <Choose>
+            <When condition={type === "level"}>等级排行榜</When>
+            <When condition={type === "recharge"}>充值排行榜</When>
+            <Otherwise>排行榜</Otherwise>
+          </Choose>
+        </span>
         <span className={styles.listStatus}>
           <TrophyGold />
           <span>恭喜中榜</span>
@@ -27,7 +50,7 @@ const RankList = ({ rankListData }) => {
         <span>名额</span>
         <span>状态</span>
       </div>
-      <For of={rankListData} each="rank" index="index">
+      <For of={fixedRankList} each="rank" index="index">
         <div className={styles.rankingRow} key={index}>
           <span>{rank?.ad_title}</span>
           <span>{rank?.reward}</span>
@@ -41,6 +64,31 @@ const RankList = ({ rankListData }) => {
           </span>
         </div>
       </For>
+      <Collapse isOpened={isOpened}>
+        <For of={collapsibleRankList} each="rank" index="index">
+          <div className={styles.rankingRow} key={index}>
+            <span>{rank?.ad_title}</span>
+            <span>{rank?.reward}</span>
+            <span>{rank?.reward_num}</span>
+            <span
+              className={classNames({
+                [styles.received]: rank?.rewarded,
+              })}
+            >
+              {rank?.rewarded ? "已领取" : "未领取"}
+            </span>
+          </div>
+        </For>
+      </Collapse>
+      <If condition={collapsibleRankList?.length > 0}>
+        <div onClick={handleToggleIsOpened} className={styles.foldBtn}>
+          {isOpened ? (
+            <ArrowDown className={styles.opened} />
+          ) : (
+            <ArrowDown className={styles.unopened} />
+          )}
+        </div>
+      </If>
     </div>
   );
 };
--- pages/my_games/[id]/rankings.js
+++ pages/my_games/[id]/rankings.js
@@ -7,7 +7,8 @@ const Rankings = ({ rank }) => {
       <div className={styles.tips}>
         最先达到指定阶梯且最先领取者可获得奖励，名额领完即止。
       </div>
-      <RankList rankListData={rank?.level} />
+      <RankList rankListData={rank?.level} type="level" />
+      <RankList rankListData={rank?.recharge} type="recharge" />
       <div className={styles.disclaimer}>
         免责声明：游戏由第三方提供并独立运营，氪金请理性。
       </div>
