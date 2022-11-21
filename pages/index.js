db3a4efee4bf31f4fe01eb7a3c820db3fe4972cf
丁松 <song.ding@idiaoyan.com>
2022-11-21 17:25:41 +0800

    change deduplicate api ref frontend

--- pages/index.js
+++ pages/index.js
@@ -45,6 +45,10 @@ const StartBtn = ({ game }) => {
             path: "",
           });
         }
+      }).catch(function(error) {
+        if (error.response.status != 200) {
+          Toast.show({ content: "该游戏不可参与，试试其他的哦" });
+        }
       });
     } else {
       callLib?.open({
